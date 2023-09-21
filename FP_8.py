332a5e2ef0e1e1e5c456bb0ad82a48b2e3452023
Samytoro <143120053+Samytoro@users.noreply.github.com>
2023-09-21 18:06:00 -0500

    Ejercicio 8
    Ejercicio 8

---
+++ FP_8.py
@@ -0,0 +1,5 @@
+num = int(input("Ingrese un número para conocer su tabla de multiplicar: \n"))
+
+for num2 in range (0,11):
+        Resultado = num * num2
+        print (num, "*", num2, "=", Resultado)
