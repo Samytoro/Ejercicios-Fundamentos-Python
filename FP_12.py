8748f74f57fbca889fde7396efc8b76bdb3b8efa
Samytoro <143120053+Samytoro@users.noreply.github.com>
2023-10-13 14:00:06 -0500

    Ejercicio 12

---
+++ FP_12.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3, 4, 5]
+print(f"Lista de números: {numeros}")
+
+numeros_delete= []
+numeros_delete.append (numeros.pop(0))
+numeros_delete.append (numeros.pop())
+
+print(f"Lista de números: {numeros}")
+print(f"Lista de números eliminados: {numeros_delete}")
